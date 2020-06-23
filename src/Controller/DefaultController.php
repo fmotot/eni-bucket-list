7b4651a759c8e712131ab74fda27828278f079e3
Guillaume <gsylvestre@gmail.com>
2020-06-23 11:55:03 +0200

    fin module 2

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller
+{
+    /**
+    * @Route("/", name="home")
+    */
+    public function home()
+    {
+        return $this->render('default/home.html.twig', []);
+    }
+
+}
