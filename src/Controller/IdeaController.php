ce44555581adb36f9bcfce82c6c164b43a481bc0
Guillaume <gsylvestre@gmail.com>
2020-06-26 12:27:30 +0200

    correction tp module 5

--- src/Controller/IdeaController.php
+++ src/Controller/IdeaController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Idea;
+use App\Repository\IdeaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,14 +14,28 @@ class IdeaController extends Controller
      */
     public function list()
     {
-        return $this->render('idea/list.html.twig', []);
+        //récupère le repository pour les Idea, afin de faire une requête SELECT
+        $repo = $this->getDoctrine()->getRepository(Idea::class);
+        //fait la requête SELECT avec un WHERE et un ORDER BY et une LIMIT à 50
+        $ideas = $repo->findBy(["isPublished" => true], ["dateCreated" => "DESC"], 50);
+
+        return $this->render('idea/list.html.twig', [
+            //passe notre variable à twig
+            "ideas" => $ideas,
+        ]);
     }
 
     /**
-     * @Route("/ideas/detail", name="idea_detail")
+     * ici j'utilisie l'injection de dépendance pour récupérer mon repo (juste pour le plaisir)
+     * @Route("/ideas/detail/{id}", name="idea_detail")
      */
-    public function detail()
+    public function detail($id, IdeaRepository $repo)
     {
-        return $this->render('idea/detail.html.twig', []);
+        //récupère une idée en fonction de sa clé primaire
+        $idea = $repo->find($id);
+        $pif = "paf";
+
+        //compact crée un tableau associatif en recherchant les variables passées sous forme de chaîne
+        return $this->render('idea/detail.html.twig', compact("idea", "pif"));
     }
 }
